45493e9e74cadcc59385c6d0d19e8c9af77a3d5f
u251355 <elena.sandin01@estudiant.upf.edu>
2025-05-01 18:15:40 +0200

    search.h

---
+++ src/search.h
@@ -0,0 +1,5 @@
+Document* load_documents_from_folder(const char* folder_path); //loads documents
+void free_documents(Document* docs); //frees the memory
+void print_document_details(const Document* doc); //prints the details of a document
+#endif // DOCUMENT_H
+
