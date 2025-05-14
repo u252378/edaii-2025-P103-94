22d761d08760b8ba37252a901b1c1a0de3bcff83
u251355 <elena.sandin01@estudiant.upf.edu>
2025-05-14 13:16:16 +0200

    reverseindex.h

---
+++ src/reverse_index.h
@@ -0,0 +1,30 @@
+#ifndef REVERSE_INDEX_H
+#define REVERSE_INDEX_H
+#include "documents.h"  
+
+typedef struct reverseIndexKey { // this struct represents a keyword 
+    char *word; // the actual keyword 
+    DocumentsList *values; // linked list of documents that contain this word
+    struct reverseIndexKey *next; // next keyword
+} ReverseIndexKey;
+
+typedef struct reverseIndexSlot { // this struct is one slot in the hash table
+    ReverseIndexKey *keys; // pointer to the first keyword in this slot 
+    int keysCount; // how many keywords are in this slot
+} ReverseIndexSlot;
+
+
+typedef struct reverseIndex {
+    int slotsCount; // total number of slots in the hash table 
+    ReverseIndexSlot **slots; // pointer to the array of slots 
+
+
+ReverseIndex *reverseIndexInit(int slots); // create and initialize the reverse index 
+void reverseIndexPut(ReverseIndex *index, char *word, DocumentsList *list); // add a word and its associated document list to the index
+
+DocumentsList *reverseIndexGet(ReverseIndex *index, char *word);// get the list of documents that contain the given word
+
+void reverseIndexFree(ReverseIndex *index, bool freeLists, bool freeDocs);
+void reverseIndexDocument(ReverseIndex *index, Document *document); // tokenize a document and add all its words to the index
+
+#endif 
