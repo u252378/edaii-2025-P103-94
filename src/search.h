04aad203f40f96bb5739e9b647fba0ca653f05dc
u252378 <anna.vargas01@estudiant.upf.edu>
2025-05-29 23:44:14 +0200

    search.h definitivo

--- src/search.h
+++ src/search.h
@@ -1,4 +1,10 @@
-Document *load_documents_from_folder(const char *folder_path); // loads documents
-void free_documents(Document *docs); // frees the memory
-void print_document_details(const Document *doc); // prints the details of a document
-#endif // DOCUMENT_H
+#ifndef SORT_SEARCH_H
+#define SORT_SEARCH_H
+#include "document.h"
+
+Document *sort_documents_by_relevance(Document *head);
+void print_sorted_documents(const Document *head);
+// Añade estas líneas al final del archivo
+float calculate_relevance(Document *doc);
+void sort_documents_list(DocumentsListNode **headRef);
+#endif
